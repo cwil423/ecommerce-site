a70281ec87992029869086044eef2f6c6f607011
Cole Williamson <williamsoncole@gmail.com>
2021-02-02 21:43:35 -0600

    removed proxy

--- src/pages/Cameras.js
+++ src/pages/Cameras.js
@@ -53,7 +53,7 @@ export default function Cameras() {
   useEffect(() => {
     let cameras = null;
     axios
-      .get("/cameras")
+      .get("https://fervent-kepler-8d0468.netlify.app/")
       .then(response => {
         cameras = response.data;
         cameras.forEach(element => {
